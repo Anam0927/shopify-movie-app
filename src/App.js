2bab9809b456a48b1b9d585fdac66cf2169874cf
Anam0927 <anam.devdes27@gmail.com>
2021-01-12 17:00:42 +0530

    installed styled-componets and favicon

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styled from 'styled-components';
 
-function App() {
+const H1 = styled.h1`
+  text-align: center;
+  font-family: sans-serif;
+  color: blue;
+  font-size: 3rem;
+`;
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <H1>Hello!</H1>
     </div>
   );
-}
+};
 
 export default App;
